d0a01274e16110c44a6f47d70afa132125c45434
Whot1966 <Whot1966@users.noreply.github.com>
2023-05-29 15:49:55 +0800

    common-full: Add font source

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,9 +3,10 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
+# Include fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+$(call inherit-product, vendor/lineage/fonts/fonts.mk)
 
 # Apps
 PRODUCT_PACKAGES += \
---
+++ fonts/fonts.mk
@@ -0,0 +1,31 @@
+# ExoTwo
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/Exotwo.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Exotwo.ttf
+
+# Google Sans
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/GoogleSans-Italic-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic-VF.ttf \
+    vendor/lineage/fonts/GoogleSans-Regular-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular-VF.ttf
+
+# Noto Sans
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/NotoSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-Italic.ttf \
+    vendor/lineage/fonts/NotoSans-Light.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-Light.ttf \
+    vendor/lineage/fonts/NotoSans-LightItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-LightItalic.ttf \
+    vendor/lineage/fonts/NotoSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-Medium.ttf \
+    vendor/lineage/fonts/NotoSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-MediumItalic.ttf \
+    vendor/lineage/fonts/NotoSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NotoSans-Regular.ttf
+
+# Roboto System
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/RobotoSystem-Regular-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/RobotoSystem-Regular-VF.ttf
+
+# Ubuntu
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/Ubuntu-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-Italic.ttf \
+    vendor/lineage/fonts/Ubuntu-Light.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-Light.ttf \
+    vendor/lineage/fonts/Ubuntu-LightItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-LightItalic.ttf \
+    vendor/lineage/fonts/Ubuntu-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-Medium.ttf \
+    vendor/lineage/fonts/Ubuntu-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-MediumItalic.ttf \
+    vendor/lineage/fonts/Ubuntu-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ubuntu-Regular.ttf
+
